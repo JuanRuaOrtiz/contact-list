32db79b04bcc9bfdb471416f5b72b8e43033b039
JuanMiguelRua <Ruaj0071@gmail.com>
2023-09-24 21:45:32 -0500

    juan

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
- let listaDeContacos = [ {
-    nombre : "juan",
-    apellido: "rua",
-  }];
+const  listaDeNombres = ["juan","sandra","junior","miguel" ] ;
+const listaDeApellidos = ["Rua","cortes","cespedes","murcia"];
+let nombreDeUsuario = [ 0,1,2,3];
+let ubicacion = [ "Bogota ,  ", "Santa marta", " manizales", "Cartagena" ];
+let contacto = [ 1234 , 6547 , 4568 , 7889 ];
+
+ alert(" usuarios resgistrados  " + nombreDeUsuario.length )
+
+for (i = 1; i < 4 ; i++ ) {
+  function Detele (){ 
+   let elegir = parseInt (prompt (' 1 para agregar \n 2 para eliminar '))
+   if (elegir === 1 ){
+    
+    for ( i=1; i<4; i++){
+      usuario = prompt( " Cual es tu nombre :")
+      listaDeNombres.push(usuario)
+      console.log(listaDeNombres)
+    }  
+  }
+    if (elegir === 2  ){
+      let A = parseInt( prompt("Que Id Quieres eliminar:"));
+
+      listaDeNombres.pop(A);
+      listaDeApellidos.pop(A);
+      nombreDeUsuario.pop(A);
+      ubicacion.pop(A);
+      contacto.pop(A);
+
+console.log("contacto eliminado")
+      
+    }
+     }
+
+} 
+
+Detele( )
+
+
+
+
   
-
-const agg =  (nombre,apellido) =>{
-  let contactoNuevo = {
-    nombre : nombre , 
-    apellido :apellido,
-  } 
-  console.log(nombre,apellido)
-
-  listaDeContacos.push(contactoNuevo);
-  console.log("contactos agragados" , contactoNuevo)
-}; 
- agg ("sandra" , "Rua" ) 
-
- const Delete = (eliminar) => {
-  listaDeContacos.splice(eliminar);
- console.log("Contacto eliminado " , eliminar)
- }
-
- Delete(0)
-
-for ( let i = 0 ; i< listaDeContacos.length ; i++){
- console.log(listaDeContacos[i])
-}
